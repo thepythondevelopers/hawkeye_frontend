884d2e21a8256ea8740733b04dfa9ad7d8cb2934
Pamsar <99356910+thepythondevelopers@users.noreply.github.com>
2022-12-19 09:22:11 +0530

    Update fb-id.component.ts

--- frontend/insights/src/app/fb-id/fb-id.component.ts
+++ frontend/insights/src/app/fb-id/fb-id.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FbAppIdService } from '../fb-app-id.service';
 import { NgToastService } from 'ng-angular-popup';
 import { Router } from '@angular/router';
+import { CheckUserPlanService } from '../check-user-plan.service';
 
 @Component({
   selector: 'app-fb-id',
@@ -10,7 +11,16 @@ import { Router } from '@angular/router';
 })
 export class FbIdComponent implements OnInit {
 
-  constructor(private router : Router,private toast:NgToastService,private fb_app_id : FbAppIdService) {
+  constructor(private cup: CheckUserPlanService,private router : Router,private toast:NgToastService,private fb_app_id : FbAppIdService) {
+
+    let email=localStorage.getItem('email');
+    this.cup.check_user_plan(email).subscribe((res:any)=>{
+      console.log("plan response=",res.user_current_plan.plan);
+      if(res.user_current_plan.plan==="Null"){
+        this.toast.info({detail:"Failure Message",summary:"Please buy a suscription first",duration:5000});
+      }
+    });
+
     if(localStorage.getItem("fb_app_id")){
       this.router.navigate(['/login-with-facebook']);  
     }
